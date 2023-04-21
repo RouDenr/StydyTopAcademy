a34aeca104ae2d60228696bedaa03129fd6ae8c9
Dennet Cordelia <36990341+RouDenr@users.noreply.github.com>
2023-04-21 16:56:25 +0000

    fin

--- 04_18/main.cpp
+++ 04_18/main.cpp
@@ -1,7 +1,76 @@
 #include <iostream>
+#include <string>
+
+class Animal{
+ private:
+    std::string name;
+
+ public:
+    Animal(std::string name) : name(name) {
+        std::cout << "Animal " << name << " was born\n";
+    }
+    Animal() : Animal("Pet") {}
+
+    virtual void voice() {
+        std::cout << "Some sounds\n";
+    }
+
+    void sayHiToAnimal(std::string mess) {
+        std::cout << "You say \"" << mess << "\" to " << this->name << "!\n";
+        /*
+            do something
+        */
+    }
+
+    ~Animal() {
+        std::cout << "Animal gone\n";
+    }
+};
+
+class Dog : public Animal {
+ public:
+    void voice() {
+        std::cout << "GAV GAV!\n";
+    }
+
+    Dog(std::string name) : Animal(name) {
+        std::cout << "Dog " << name << " was born\n";
+    }
+    Dog() : Animal() {}
+
+    ~Dog() {
+        std::cout << "Dog gone\n";
+    }
+};
+
+
+class Cat : public Animal {
+ public:
+    void voice() {
+        std::cout << "maw maw!\n";
+    }
+
+    Cat(std::string name) : Animal(name) {
+        std::cout << "Cat " << name << " was born\n";
+    }
+    Cat() : Animal() {}
+
+    ~Cat() {
+        std::cout << "Cat gone\n";
+    }
+};
+
+void saySomeToAnimal(Animal *animal) {
+    animal->voice();
+    animal->sayHiToAnimal("Hi");
+}
+
 
 int main() {
-    std::cout << "Hello Github!!!\n";
-    std::cout << "Again bay!!\n";
+    Dog some_dog("Шарик");
+    Cat some_cat("Киса");
+
+    saySomeToAnimal(&some_dog);
+    saySomeToAnimal(&some_cat);
     return 0;
 }
---
+++ 04_21/main.cpp
@@ -0,0 +1,127 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <ios>
+
+void inpun_int() {
+    int n;
+
+    // ifstream - input file stream (поток файла для чтения)
+    std::ifstream test("test.txt");
+    test >> n;
+    std::cout << n << "\n";
+}
+
+void output_int() {
+    int n = 44;
+
+    // ofstream - output file stream (поток файла для записи)
+    // std::ios::app - флаг для дозаписи, иначе файл перезапишеться
+    // std::ofstream test("test.txt", std::ios::app);
+    std::ofstream test("test.txt");
+
+    test << n << "\n";
+    test << "Hello world!!\n";
+}
+
+void readline_file(const std::string &filename) {
+    std::ifstream fin(filename);
+    int n;
+    std::string str;
+
+    std::string line;
+    std::getline(fin, line);
+    std::cout << line << "\n";
+
+    fin >> n;
+    std::cout << n;
+    std::getline(fin, str);
+    std::cout << str << "\n";
+    std::getline(fin, str);
+    std::cout << str << "\n";
+}
+
+std::string inpun_line() {
+    std::string line;
+    std::getline(std::cin, line);
+    return line;
+}
+
+void print_all_file(const std::string &filename) {
+    std::ifstream fin(filename);
+
+    if (not fin.good()) {
+        std::cout << "file error\n";
+        return;
+    }
+
+    while (not fin.eof()) {
+        std::string line;
+        std::getline(fin, line);
+        std::cout << line << "\n";
+    }
+}
+
+// void out_recd()
+
+void read_all(std::istream &in) {
+    if (not in.good()) {
+        std::cout << "file error\n";
+        return;
+    }
+
+    while (not in.eof()) {
+        std::string line;
+        std::getline(in, line);
+        std::cout << line << "\n";
+    }
+}
+
+void test_string_stream() {
+    std::stringstream sio;
+
+    int n, m;
+    std::string tmp = "4 5";
+
+    sio << tmp;
+
+    sio >> n >> m;
+    std::cout << n + m << "\n";
+}
+
+void add_out_rect(std::ostream &out) {
+    for (int i = 0; i < 5; ++i) {
+        for (int j = 0; j < 5; ++j) {
+            out << "0";
+        }
+        out << "\n";
+    }
+}
+
+int main(int argc, char const *argv[])
+{
+    // std::ofstream test("test.txt");
+    // output_int();
+    // readline_file("test.txt");
+    // std::cout << "File: ";
+    // print_all_file(inpun_line());
+
+    // std::ifstream  in(inpun_line());
+
+    std::stringstream sio;
+
+    // sio << "Hello!!\nMy name is Ivan!\n";
+    // sio << 42 << " " << 55 + 5 << "\n";
+
+    // read_all();
+
+
+    std::ofstream  out(inpun_line());
+
+
+    add_out_rect(sio);
+    read_all(sio);
+
+    std::cout << "END!!\n";
+    return 0;
+}

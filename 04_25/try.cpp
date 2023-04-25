d5cfeea5962a3b4d6c93dc7a0d9aa53f1cd0deea
Dennet Cordelia <36990341+RouDenr@users.noreply.github.com>
2023-04-25 15:29:28 +0000

    try

--- 04_25/try.cpp
+++ 04_25/try.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 
-// Если функция вернула отрицательное число
+// Если функция вернула -1
 // То Это ошибка ввода пользователя
+// Если функция вернула -2
+// То Это ошибка деление на ноль
 int input_n(int *n) {
-    std::cout << "Введите число от 15 до 45";
+    std::cout << "Введите число от 15 до 45 ";
     std::cin >> *n;
 
     if (*n < 15 || *n > 45) {
@@ -16,7 +18,10 @@ int del_func(int *n) {
     int m;
     std::cout << "Введите";
     std::cin >> m;
+    if (m == 0)
+        return -2;
     std::cout  << "n/m =" << *n / m;
+    return 0;
 }
 
 int main(int argc, char const *argv[])
@@ -28,9 +33,14 @@ int main(int argc, char const *argv[])
     return_val = input_n(n);
     if (return_val < 0) {
         delete n;
-        std::cout << "Error input!";
+        std::cout << "Error input!\n";
     } else {
-        std::cout << "Ok!";
+        return_val = del_func(n);
+        if (return_val == -2) {
+            std::cout << "Error div zero!\n";
+
+        }
+        std::cout << "Ok!\n";
     }
 
     //
